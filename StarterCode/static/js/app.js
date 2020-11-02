e5fbb4189c4fef162695343449e6537f0b0c6f71
Hannah <hannah@Hannahs-MacBook-Pro.local>
2020-11-01 20:47:59 -0600

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,41 +1,88 @@
 dropdown = d3.select("#selDataset");
+meta = d3.select("#sample-metadata");
 
 d3.json("samples.json").then(processData);
 
 function processData(data) {
+
     console.log(data);
+    metadata = data.metadata;
     names = data.names;
     samples = data.samples;
     names.forEach( function (name) {
         dropdown.append("option").text(name).property(name);
     })
 
-    function grab(id) {
-        filtered = samples.filter(d => d.id == id);
-        console.log(filtered);
-        otus = filtered.map(row => row.otu_ids);
-        sample_values = filtered.map(row => row.sample_values);
-        sorted = sample_values.sort((first,second) => second-first);
+    function plot(id) {
+        filteredMeta = metadata.filter(d => d.id == id)[0];
+        filteredSamples = samples.filter(d => d.id == id)[0];
         
-        var trace = {
-           y: sorted,
-           x: otus,
-           orientation: "h",
-           type: "bar"
-        }
+        meta.html(`id: ${id} <br> 
+            ethnicity: ${filteredMeta.ethnicity} <br>
+            gender: ${filteredMeta.gender} <br>
+            age: ${filteredMeta.age} <br>
+            location: ${filteredMeta.location} <br>
+            bbtype: ${filteredMeta.bbtype} <br>
+            wfreq: ${filteredMeta.wfreq} <br>`)
 
-        Plotly.newPlot("bar",[trace])
+        otus = filteredSamples.otu_ids;
+        //QUESTION: Can't get anonymous function to return OTU id to otus or new list
+        labels = otus.forEach(function(otu) {
+            console.log("OTU " + String(otu))
+        });
+        console.log(labels);
+        hover = filteredSamples.otu_labels;
+        console.log(hover);
+        sample_values = filteredSamples.sample_values;
+        sample_values = sample_values.sort((first,second) => second-first);
         
+        sample_values = sample_values.slice(0,10);
+        otus = otus.slice(0,10);
+        hover = hover.slice(0,10);
+        console.log(sample_values);
+
+        var trace = {
+           x: sample_values,
+           y: otus,
+           text: hover,
+           orientation: "h",
+           type: "bar",
+        };
+
+        var bubble = {
+            x: otus,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otus
+            },
+            text: hover
+        };
+
+        var layout = {
+            xaxis: {
+                autorange: true
+            }
+        };
+
+        // QUESTION: Plotly.restyle("bar", [trace], layout) doesn't work!
+        Plotly.newPlot("bar",[trace],layout);
+        Plotly.newPlot("bubble",[bubble],layout);
+
     };
 
-    dropdown.on("change", handle)
+    function init() {
+        plot(940);
+    };
+
+    init(940);
+
+    dropdown.on("change", handle);
 
     function handle() {
-        console.log("Handled!")
-        input = dropdown.property("value")
-        grab(input)
+        input = dropdown.property("value");
+        plot(input);
         
     }
-
-
 }
