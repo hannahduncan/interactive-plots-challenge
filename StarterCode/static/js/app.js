49687b1b7af5819b99118b3c66c4a248a462b7d9
Hannah <hannah@Hannahs-MacBook-Pro.local>
2020-11-03 07:47:31 -0600

    fixes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,21 +1,21 @@
-dropdown = d3.select("#selDataset");
-meta = d3.select("#sample-metadata");
+var dropdown = d3.select("#selDataset");
+var meta = d3.select("#sample-metadata");
 
 d3.json("samples.json").then(processData);
 
 function processData(data) {
 
-    console.log(data);
-    metadata = data.metadata;
-    names = data.names;
-    samples = data.samples;
+    // console.log(data);
+    var metadata = data.metadata;
+    var names = data.names;
+    var samples = data.samples;
     names.forEach( function (name) {
         dropdown.append("option").text(name).property(name);
     })
 
     function plot(id) {
-        filteredMeta = metadata.filter(d => d.id == id)[0];
-        filteredSamples = samples.filter(d => d.id == id)[0];
+        var filteredMeta = metadata.filter(d => d.id == id)[0];
+        var filteredSamples = samples.filter(d => d.id == id)[0];
         
         meta.html(`id: ${id} <br> 
             ethnicity: ${filteredMeta.ethnicity} <br>
@@ -25,26 +25,32 @@ function processData(data) {
             bbtype: ${filteredMeta.bbtype} <br>
             wfreq: ${filteredMeta.wfreq} <br>`)
 
-        otus = filteredSamples.otu_ids;
-        //QUESTION: Can't get anonymous function to return OTU id to otus or new list
-        labels = otus.forEach(function(otu) {
-            console.log("OTU " + String(otu))
+        var otus = filteredSamples.otu_ids;
+
+        var labels = otus.toString()
+        labels = labels.split(",");
+        stuff = []
+        labels.forEach(function(otu) {
+            stuff.push("OTU " + otu)
         });
-        console.log(labels);
-        hover = filteredSamples.otu_labels;
-        console.log(hover);
-        sample_values = filteredSamples.sample_values;
-        sample_values = sample_values.sort((first,second) => second-first);
-        
-        sample_values = sample_values.slice(0,10);
-        otus = otus.slice(0,10);
-        hover = hover.slice(0,10);
-        console.log(sample_values);
+        // console.log(stuff);
+
+        var hover = filteredSamples.otu_labels;
+        // console.log(hover);
+
+        var sample_values = filteredSamples.sample_values;
+        // sample_values = sample_values.sort((first,second) => second-first);
+        // console.log(sample_values);
+
+        var sliced_values = sample_values.slice(0,10);
+        var sliced_stuff = stuff.slice(0,10);
+        var sliced_hover = hover.slice(0,10);
+        // console.log(sliced_values);
 
         var trace = {
-           x: sample_values,
-           y: otus,
-           text: hover,
+           x: sliced_values,
+           y: sliced_stuff,
+           text: sliced_hover,
            orientation: "h",
            type: "bar",
         };
@@ -55,20 +61,32 @@ function processData(data) {
             mode: "markers",
             marker: {
                 size: sample_values,
-                color: otus
+                color: otus,
+                colorscale: "YlGnBu"
             },
             text: hover
         };
 
-        var layout = {
+        var bar_layout = {
             xaxis: {
+                autorange: true
+            },
+            yaxis: {
                 autorange: true
             }
         };
 
-        // QUESTION: Plotly.restyle("bar", [trace], layout) doesn't work!
-        Plotly.newPlot("bar",[trace],layout);
-        Plotly.newPlot("bubble",[bubble],layout);
+        var bubble_layout = {
+            xaxis: {
+                autorange: true
+            },
+            yaxis: {
+                autorange: true
+            }
+        };
+
+        Plotly.newPlot("bar",[trace],bar_layout);
+        Plotly.newPlot("bubble",[bubble],bubble_layout);
 
     };
 
@@ -76,12 +94,12 @@ function processData(data) {
         plot(940);
     };
 
-    init(940);
+    init();
 
     dropdown.on("change", handle);
 
     function handle() {
-        input = dropdown.property("value");
+        var input = dropdown.property("value");
         plot(input);
         
     }
